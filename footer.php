dd810e9f6aa41f51348c0c980f2b1d709d5e9f90
FernandoPY <35412838+1531191@users.noreply.github.com>
2021-02-24 10:31:59 -0500

    Version 4.0

---
+++ footer.php
@@ -0,0 +1,104 @@
+<footer id="site-footer" class="site-footer site-footer-2 site-footer-5">
+    <div class="main-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget">
+                        <img src="images/logo.png" class="attachment-full size-full m-b25" alt="">
+                        <p class="m-b30">Our approach to SEO is uniquely built around what we know works…and what we know doesn’t work. With over 200 verified factors in play.</p>
+                        <div class="footer-social">
+                            <a class="footer-social-icon twitter" href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+                            <a class="footer-social-icon facebook" href="#" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                            <a class="footer-social-icon linkedin" href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                            <a class="footer-social-icon instagram" href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget">
+                        <h5>Contacto</h5>
+                        <div class="footer-contact-2">
+                            <ul class="font-second">
+                                <li><strong>Address:</strong> Palmares , Costa Rica <br></li>
+                                <li><strong>Email:</strong> info@avancescr.com</li>
+                                <li><strong>Phone:</strong> (506) 4035-6809</li>
+                                <li><strong>Soporte:</strong> soporte@avancescr.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget">
+                        <h5>Servicios</h5>
+                        <ul class="ft-menu-list">
+                            <li class="ft-menu-item">
+                                <a href="#">SEO for Small Business</a>
+                            </li>
+                            <li class="ft-menu-item">
+                                <a href="#">SEO for Local Services</a>
+                            </li>
+                            <li class="ft-menu-item">
+                                <a href="#">Enterprise SEO</a>
+                            </li>
+                            <li class="ft-menu-item">
+                                <a href="#">National SEO</a>
+                            </li>
+                            <li class="ft-menu-item">
+                                <a href="#">International SEO</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget">
+                        <h5>Certificaciones</h5>
+                        <div class="widget-insta-feeds instafeed-gallery">
+                            <div class="instafeed-item">
+                            <img src="img/bni.png" alt="">
+                            </div>
+                            <div class="instafeed-item">
+                                <img src="img/image-(1).png" alt="">
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <p class="copyright-2 text-center">Copyright © 2021 Avantec.</p>
+        </div>
+    </div>
+</footer><!-- #site-footer -->
+</div><!-- #page -->
+<a id="back-to-top" href="#" class="show"><i class="flaticon-arrow-pointing-to-up"></i></a>
+        <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/easypiechart.js"></script>
+    <script src="js/particles.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/scripts.js"></script>
+    <script src="js/header-mobile.js"></script>
+    <script src="js/royal_preloader.min.js"></script>
+</body>
+</html>
+<script type="text/javascript">
+    window.jQuery = window.$ = jQuery;  
+    (function($) { "use strict";
+        //Preloader
+        Royal_Preloader.config({
+            mode           : 'logo',
+            logo           : 'img/logo.png',
+            logo_size      : [110, 80],
+            showProgress   : true,
+            showPercentage : true,
+            text_colour: '#000000',
+            background:  '#ffffff'
+        });
+    })(jQuery);
+</script> 
